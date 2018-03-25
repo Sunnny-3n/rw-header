b7a739488c9320f978111ed02c59e221f085f8be
Sunnny-3n <amy2819106462@gmail.com>
2018-03-26 00:07:13 +0800

    核心免流代码

--- src/rewrite.c
+++ src/rewrite.c
@@ -1,1 +1,94 @@
 #include "rewrite.h"
+
+static void init_hash_table(uint64_t hash_table[],int table_size,va_list args)
+{
+    for(int i = 0;i < table_size;i++){
+        char * key = va_arg(args,char *);
+        hash_table[i] = hash(key,strlen(key));
+    }
+
+    va_end(args);
+}
+
+static bool is_in_hash_table(uint64_t hash_table[],int table_size,uint64_t hash)
+{
+    for(int i = 0;i < table_size;i++){
+        if(hash_table[i] == hash)
+            return true;
+    }
+
+    return false;
+}
+
+/*将 headers(除了可变参数中的字段名及其参数) 打印到 buf 中去,返回成功打印的字符数
+*
+* -----------headers-----------
+     name       |      value
+*   User-Agent  | Mozilla/5.0 
+*   Connection  | keep-alive
+*   Host        | secure.gravatar.com:443
+*
+* e.g. snprintf_other_headers(buf,headers,4,2,"Host","User-Agent")
+* 打印后 buf 中的内容
+* Connection: keep-alive\r\n
+*/
+static ssize_t snprintf_other_headers(struct bufer * buf,
+                                      struct phr_header headers[],              size_t headers_size,
+                                      int blacklist_count,...)
+{
+    ssize_t n = 0;
+    va_list args;
+    va_start(args,blacklist_count);
+
+    uint64_t hash_table[blacklist_count];
+    init_hash_table(hash_table,blacklist_count,args);
+
+    for(int i = 0;i < headers_size;i++){
+        uint64_t hash_sum = hash(headers[i].name,headers[i].name_len);
+
+        if(!is_in_hash_table(hash_table,blacklist_count,hash_sum)){
+            n += snprintf(buf->buf + n,max_bufsize - n,"%.*s: %.*s\r\n",
+                          (int)headers[i].name_len,headers[i].name,
+                          (int)headers[i].value_len,headers[i].value);
+        }
+    }
+    buf->len = n;
+    return n;
+}
+
+#define pos(name) \
+pos_header_field(src_header.headers,src_header.headers_size,name,strlen(name))
+
+#define value(name) (int)pos(name)->value_len,pos(name)->value
+
+#define COMMON                         \
+    struct bufer src_buf,              \
+                 tmp_buf;              \
+                                       \
+    struct header src_header;          \
+    struct bufer out;                  \
+                                       \
+    readall(&src_buf,srcfd);           \
+    if(src_buf.len <= 0){              \
+        log_error("Buffer no data");   \
+        return;                        \
+    }                                  \
+    parse_req(src_buf,&src_header);    \
+
+void rewrite_HTTPS_header_and_send(int srcfd,int dstfd)
+{
+    //宏
+    COMMON
+    snprintf_other_headers(&tmp_buf,src_header.headers,src_header.headers_size,                        HTTPS_DEL_COUNT,HTTPS_DEL);
+    out.len = snprintf(out.buf,max_bufsize,HTTPS_RW);
+    writeall(out,dstfd);
+}
+
+void rewrite_HTTP_header_and_send(int srcfd,int dstfd)
+{
+    //宏
+    COMMON
+    snprintf_other_headers(&tmp_buf,src_header.headers,src_header.headers_size,                        HTTPS_DEL_COUNT,HTTP_DEL);
+    out.len = snprintf(out.buf,max_bufsize,HTTP_RW);
+    writeall(out,dstfd);
+}
--- src/rewrite.h
+++ src/rewrite.h
@@ -4,4 +4,44 @@
 #include "httpparser.h"
 
 
+
+//method
+#define METHOD (int)src_header.method_len,src_header.method
+//url
+#define URL (int)src_header.path_len,src_header.path
+#define OTHER_HEADERS tmp_buf.buf
+
+//用于免流,删除 Host 头等
+//删除请求头的个数
+#define HTTPS_DEL_COUNT 1
+#define HTTPS_DEL "Host"
+
+//CONNECT / \nrd.go.10086.cn HTTP/1.0\n\thttp://[H]\rHost:[H]\nHost: rd.go.10086.cn\n
+#define HTTPS_RW                          \
+"CONNECT %.*s HTTP/1.1\r\n"                           \
+"X-Online-Host: rd.go.10086.cn\r\n"                      \
+"Connection: keep-alive\rX-Online-Host: %.*s\r\n"          \
+"Host: rd.go.10086.cn\r\n"                        \
+"%s"                                               \
+"\r\n"                                              \
+,value("Host"),value("Host"),OTHER_HEADERS         \
+
+
+#define HTTP_DEL_COUNT 1
+#define HTTP_DEL "Host"
+
+//[M] [U] [V]\n\thttp://[H]\rHost:[H]\r\nHost: rd.go.10086.cn\r\n
+#define HTTP_RW                      \
+"%.*s %.*s HTTP/1.1\n"               \
+"\thttp://%.*s\rHost:%.*s\r\n"       \
+"Host: rd.go.10086.cn\r\n"           \
+"%s"                                 \
+"\r\n"                               \
+,METHOD,URL,value("Host"),value("Host"),OTHER_HEADERS
+
+
+
+void rewrite_HTTPS_header_and_send(int srcfd,int dstfd);
+void rewrite_HTTP_header_and_send(int srcfd,int dstfd);
+
 #endif
